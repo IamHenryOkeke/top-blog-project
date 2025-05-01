e37665ae0375581941c3045d034bb4f6d88f73f2
Okeke Nmesomachukwu Henry <Iamhenryokeke@gmail.com>
2025-05-01 10:49:25 +0100

    updated auth zod schema

--- backend/src/utils/schemas.ts
+++ backend/src/utils/schemas.ts
@@ -2,8 +2,14 @@ import { z } from "zod";
 
 export const createUserSchema = z.object({
   name: z.string().min(3),
-  email: z.string().email({message: "Email must be valid"}),
-  password: z.string().min(6),
+  email: z.string({message: "Email is required"}).email({message: "Email must be valid"}),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long" })
+    .regex(/[A-Z]/, { message: "Password must contain at least one uppercase letter" })
+    .regex(/[a-z]/, { message: "Password must contain at least one lowercase letter" })
+    .regex(/[0-9]/, { message: "Password must contain at least one number" })
+    .regex(/[^A-Za-z0-9]/, { message: "Password must contain at least one special character" })
 });
 
 export const loginUserSchema = z.object({
@@ -15,7 +21,7 @@ export const sendOTPSchema = loginUserSchema.pick({
   email: true
 })
 
-export const resetPassswordSchema = loginUserSchema.merge(z.object({
+export const resetPassswordSchema = createUserSchema.omit({name: true}).merge(z.object({
   otp: z
       .string()
       .length(6, "OTP must be 6 digits")
