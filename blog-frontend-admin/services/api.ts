38b75a109fd0992602476e44013774c48edae11b
Okeke Nmesomachukwu Henry <Iamhenryokeke@gmail.com>
2025-05-16 11:06:34 +0100

    added url config for diff node env

--- blog-frontend-admin/services/api.ts
+++ blog-frontend-admin/services/api.ts
@@ -1,8 +1,10 @@
 import useAuthStore from "@/store/auth";
 import axios from "axios";
 
+const baseURL = process.env.NODE_ENV === "production" ? process.env.NEXT_PUBLIC_API_URL : process.env.NEXT_PUBLIC_API_URL_DEV;
+
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL, 
+  baseURL, 
   headers: {
     "Content-Type": "application/json",
   },
